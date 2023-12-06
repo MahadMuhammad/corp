7ba8eb910544f2303dff83a3ae7a6707b046e656
Muhammad Mahad <mahadtxt@gmail.com>
2023-12-06 11:45:25 +0500

    Manager Close Project Done

--- src/main/java/com/example/sdacorp/ManagerCloseProjectController.java
+++ src/main/java/com/example/sdacorp/ManagerCloseProjectController.java
@@ -2,17 +2,36 @@ package com.example.sdacorp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
+import javafx.util.Pair;
 
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class ManagerCloseProjectController {
+
+public class ManagerCloseProjectController implements Initializable {
 
     @FXML
     private Label forclosing;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private ListView<String> listview;
+
+    @FXML
+    private TextField pid;
 
     ManagerModel managerModel = new ManagerModel();
 
@@ -44,6 +63,37 @@ public class ManagerCloseProjectController {
 
     }
 
+    @FXML
+    void OncClickCloseBtn(ActionEvent event) {
+
+        if (pid.getText().isEmpty()) {
+            label.setText("Please fill all the fields");
+        } else {
+            int project_id = Integer.parseInt(pid.getText());
+            try {
+                ProjectMain.ProjectCRUD projectCRUD = new ProjectMain.ProjectCRUD();
+                ProjectMain.Project project;
+                if((project = projectCRUD.getProject(project_id)) == null){
+                    label.setText("Project Not Found");
+                    return;
+                }
+                if (Objects.equals(project.getStatus(), "Closed")){
+                    label.setText("Project Already Closed");
+                    return;
+                }
+                project.setStatus("Closed");
+                try {
+                    projectCRUD.updateProject(project);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                label.setText("Project Closed Successfully");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     @FXML
     void OncClickCloseProjectBtn(ActionEvent event) throws IOException{
@@ -60,5 +110,27 @@ public class ManagerCloseProjectController {
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            ProjectMain.ProjectCRUD projectCRUD = new ProjectMain.ProjectCRUD();
+            List<Pair<Integer,String>> allprojects = projectCRUD.getAllProject();
+            // assign the data to the table
 
+            for (Pair<Integer,String> project : allprojects) {
+                String s = "\t\t" +project.getKey() + "\t\t\t\t\t\t\t\t\t\t" + project.getValue() + " \n";
+                listview.getItems().add(s);
+            }
+
+
+
+
+
+
+
+            //project_table = (TableColumn<Integer, String>) projectCRUD.getAllProject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
